11fa53589bea19151ea0598019dae5f4927fec29
kawisogod <kkawiso3@gmail.com>
2022-07-11 01:04:58 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * @n: length of line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
