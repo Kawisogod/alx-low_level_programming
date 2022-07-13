eb5f33b46785497b15dd1af035b3f0587ed52ad7
kawisogod <kkawiso3@gmail.com>
2022-07-13 03:42:05 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,55 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * _putchar - writes the character c to stdout
- */
-int _putchar(char c);
-/**
- * _isupper - checks for uppercase character
- */
+void print_triangle(int size);
 int _isupper(int c);
-/**
- * _isdigit - checks for digit (0 through 9)
- */
 int _isdigit(int c);
-/**
- * mul - multiplies two integers
- */
 int mul(int a, int b);
-/**
- * print_numbers - prints the numbers from 0 to 9
- */
-void print_numbers(void);
-/**
- * print_most_numbers - print numbers from 0 to 9 except 2 and 4
- */
 void print_most_numbers(void);
-/**
- * print_line - draws a straight line in the terminal
- */
-void print_line(int n);
-/**
- * print_diagonal - draws a diagonal line on the terminal
- */
-void print_diagonal(int n);
-/**
- * more_numbers - prints 10 times the numbers form 0 to 14
- */
 void more_numbers(void);
-/**
- * print_square - prints a square followed by a new line
- */
+void print_line(int n);
+void print_diagonal(int n);
 void print_square(int size);
-/**
- * fizz_buzz - prints numbers from 1 to 100 followed by a new line
- * for multiples of three print Fizz
- * for multiples of five print Buzz
- * for numbers that are multiples of both three and five print FizzBuzz
- */
-
-/**
- * print_triangle - prints a triangle followed by a new line
- */
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_alphabet(void);
+void print_times_table(int n);
