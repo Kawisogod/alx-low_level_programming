447dc31f554f4ce7302bf0c81382d1b00729c641
kawisogod <kkawiso3@gmail.com>
2022-07-13 05:55:56 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -26,9 +26,9 @@ void print_times_table(int n);
  * main - main function
  * checks through numbers from 1 - 100
  * and print number if it is not a multiple of 3 or 5
- * print fizz if its a multiple of 3
- * buzz if its a multiple of 5
- * and fizz buzz if its both a multiple of 3 and 5
+ * print Fizz if its a multiple of 3
+ * Buzz if its a multiple of 5
+ * and FizzBuzz if its both a multiple of 3 and 5
  *
  * Created by: Sanctus-Peter
  * cc: 7th july, 2022
