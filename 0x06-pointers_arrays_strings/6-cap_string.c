69a02d2933bbc380de03e8c788f8b566267d9a81
kawisogod <kkawiso3@gmail.com>
2022-07-14 08:49:49 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,30 +12,29 @@ int i, j;
 int hasWord;
 char separators[] = ",;.!?(){}\n\t\" ";
 
-	for (i = 0, hasWord = 0; str[i] != '\0'; i++)
-	{
-		if (str[0] >= 'a' && str[0] <= 'z')
-			hasWord = 1;
+for (i = 0, hasWord = 0; str[i] != '\0'; i++)
+{
+if (str[0] >= 'a' && str[0] <= 'z')
+hasWord = 1;
 
-		for (j = 0; separators[j] != '\0'; j++)
-		{
-			if (separators[j] == str[i])
-				hasWord = 1;
-		}
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (separators[j] == str[j])
+hasWord = 1;
+}
+if (hasWord)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= ('a' - 'A');
+hasWord = 0;
+}
+else if (str[i] >= 'A' && str[i] <= 'Z'
+hasWord = 0;
+else if (str[i] >= '0' && str[i] <= '9')
+hasWord = 0;
+}
+}
 
-		if (hasWord)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] -= ('a' - 'A');
-				hasWord = 1;
-			}
-			else if (str[i] >= 'A' && str[i] <= 'Z')
-				hasWord = 0;
-			else if (str[i] >= '0' && str[i] <= '9')
-				hasWord = 0;
-		}
-	}
-
-	return (str);
+return (str);
 }
